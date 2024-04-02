c6d23219b2ddd595c4a41a96eab18b03c6b5f968
brian110326 <brian110326@naver.com>
2024-04-02 17:04:39 +0900

    Validation

--- web1/src/main/java/com/example/web1/dto/LoginDto.java
+++ web1/src/main/java/com/example/web1/dto/LoginDto.java
@@ -1,5 +1,8 @@
 package com.example.web1.dto;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
@@ -11,9 +14,11 @@ import lombok.ToString;
 
 @Data
 public class LoginDto {
+    @Email(message = "이메일을 확인해주세요")
     @NotEmpty // @NotNull + 비어있는값 불가
     private String email;
 
-    @NotBlank // @NotEmpty + 비어있는값 불가
+    @Length(min = 2, max = 6)
+    // @NotBlank // @NotEmpty + 비어있는값 불가
     private String name;
 }
