d5229f4b5586b26581dcd49a36c3c725a727f893
brian110326 <brian110326@naver.com>
2024-04-09 16:20:49 +0900

    Book

--- book/src/main/java/com/example/book/controller/BookController.java
+++ book/src/main/java/com/example/book/controller/BookController.java
@@ -4,14 +4,21 @@ import org.springframework.stereotype.Controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @Log4j2
+@RequestMapping("/book")
 public class BookController {
-    @GetMapping("path")
-    public String getMethodName(@RequestParam String param) {
-        return new String();
+    @GetMapping("/list")
+    public String listGet() {
+        return "/book/list";
+    }
+
+    @GetMapping("/create")
+    public void createGet() {
+        log.info("/book/create 주소 요청");
     }
 
 }
