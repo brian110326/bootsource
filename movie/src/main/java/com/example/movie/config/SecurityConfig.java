3fe479519703590a25489ea885f9dd214f77774b
brian110326 <brian110326@naver.com>
2024-05-02 17:50:20 +0900

    member

--- movie/src/main/java/com/example/movie/config/SecurityConfig.java
+++ movie/src/main/java/com/example/movie/config/SecurityConfig.java
@@ -20,9 +20,14 @@ public class SecurityConfig {
                 // 로그인 하기 전 어디 영역까지 보여줄 것인지 설정
                 .requestMatchers("/", "/assets/**", "/css/**", "/js/**").permitAll()
                 .requestMatchers("/movie/list", "/movie/read").permitAll()
+                // 이미지 안보이는 것 해결
                 .requestMatchers("/upload/display").permitAll()
+                // script에서 restcontroller /reviews 해결
                 .requestMatchers("/reviews/**").permitAll()
                 .anyRequest().authenticated());
+
+        // 로그인 페이지는 /member/login 경로 요청 해야함
+        http.formLogin(login -> login.loginPage("/member/login").permitAll());
 
         // remove post시 에러나서 임시방편으로 csrf해제(form의 action값을 안줘서)
         http.csrf(csrf -> csrf.disable());
---
+++ movie/src/main/java/com/example/movie/controller/MemberController.java
@@ -0,0 +1,25 @@
+package com.example.movie.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.movie.dto.PageRequestDto;
+
+@RequestMapping("/member")
+@Controller
+@Log4j2
+public class MemberController {
+
+    // 리다이렉션 횟수가 너무 많습니다 ==> @ModelAttribute("requestDto") PageRequestDto
+    // pageRequestDto 추가하기
+    @GetMapping("/login")
+    public void getLogin(@ModelAttribute("requestDto") PageRequestDto pageRequestDto) {
+        log.info("로그인 폼 요청");
+    }
+
+}
